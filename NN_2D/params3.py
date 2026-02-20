70510a96c0754c21e2f55e14ae0041df976a2207
Jasmin Sternkopf <jasmin.sternkopf@mathematik.tu-chemnitz.de>
2026-02-20 23:06:49 +0100

    deleting old

--- NN_2D/params3.py
+++
@@ -1,6 +0,0 @@
-from params import *
-
-datasetsM = [datasetsM[0]]
-datasetsname_short = [datasetsname_short[0]]
-archs = [archs[-1]]
-epochs = [epochs[-1]]
--- NN_2D/params4.py
+++
@@ -1,6 +0,0 @@
-from params import *
-
-datasetsM = [datasetsM[1]]
-datasetsname_short = [datasetsname_short[1]]
-archs = [archs[-1]]
-epochs = [epochs[-1]]
